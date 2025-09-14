4a8549522322d842381212aee671a29ac337c6e8
Siraaj_Wilkinson <siraajw19@gmail.com>
2025-09-14 16:22:59 +0200

    second workflow added

--- workflows/src/test/java/com/pipline/workflows/WorkflowsApplicationTests.java
+++ workflows/src/test/java/com/pipline/workflows/WorkflowsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.pipline.workflows;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WorkflowsApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
+	
+	
+	@Test
+	public void testPipeline() {
+		System.out.println("first test");
+		assertTrue(true);
+	}
 }
