9625cd48eaf728b37c88e700dc5a6cafccf33c3f
Siraaj_Wilkinson <siraajw19@gmail.com>
2025-09-14 16:49:17 +0200

    test workflow

--- workflows/src/main/java/com/pipline/workflows/WorkflowsApplication.java
+++ workflows/src/main/java/com/pipline/workflows/WorkflowsApplication.java
@@ -8,6 +8,7 @@ public class WorkflowsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowsApplication.class, args);
+		System.out.println("Testing");
 	}
 
 }
